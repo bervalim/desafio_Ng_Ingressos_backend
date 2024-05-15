be555cb3d4a2c599952a357f73b7f9ba8148c85f
bervalim <bernardogvalim@gmail.com>
2024-05-15 12:10:21 -0300

    feat/handle error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
-import express from "express";
+import "reflect-metadata";
+import "express-async-errors";
+import cors from "cors";
+import express, { Application } from "express";
+import { handleErrors } from "./middlewares/HandleErrors.middleware";
 
-export const app = express();
+export const app: Application = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:4200"],
+  })
+);
+
+app.use(handleErrors);
