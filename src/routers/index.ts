fa486d7b531da088c25bea9adbc7e2b754fc8305
bervalim <bernardogvalim@gmail.com>
2024-05-16 03:13:15 -0300

    feat/posts router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { userRouter } from "./user.router";
 import { sessionRouter } from "./session.router";
+import { postRouter } from "./post.router";
 
 export const allRoutes: Router = Router();
 allRoutes.use("/users", userRouter);
 allRoutes.use("/login", sessionRouter);
+allRoutes.use("/posts", postRouter);
