4b6be63ad658e1782c77f308462df6d2ee789b63
bervalim <bernardogvalim@gmail.com>
2024-05-16 04:44:06 -0300

    feat/crud post finished

--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -5,7 +5,7 @@ const SexEnumValues = ["Male", "Female", "Other"] as const;
 export const postResponseSchema = z.object({
   id: z.string(),
   title: z.string().min(3).max(120),
-  content: z.string().email().min(3),
+  content: z.string().min(3),
   createdAt: z.date(),
   user_id: z.string(),
 });
@@ -15,13 +15,12 @@ export const readPostsResponseSchema = postResponseSchema.array();
 export const readOnePostResponseSchema = z.object({
   id: z.string(),
   title: z.string().min(3).max(120),
-  content: z.string().email().min(3),
+  content: z.string().min(3),
   createdAt: z.date(),
   user_id: z.string(),
   user: z.object({
     name: z.string().min(3).max(80),
     email: z.string().email().min(3).max(45),
-    password: z.string().max(120).min(8),
     birthDate: z.string().max(30).min(1),
     sex: z.enum(SexEnumValues),
     avatar: z.string().max(200).optional(),
