5b313b5c1f798c0a6baf03f03a98a036bd293635
bervalim <bernardogvalim@gmail.com>
2024-05-17 00:00:24 -0300

    feat/exclude helper.ts

--- src/services/helpers.ts
+++
@@ -1,4 +0,0 @@
-export const isValidImageUrl = (url: string): boolean => {
-  const imageExtensions = [".jpg", ".jpeg", ".png", ".gif"];
-  return imageExtensions.some((ext) => url.toLowerCase().endsWith(ext));
-};
